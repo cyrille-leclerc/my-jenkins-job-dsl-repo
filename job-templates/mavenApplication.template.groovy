dc6e925348360a9a460628964fc7985411b685c8
Cyrille Le Clerc <cyrille@cyrilleleclerc.com>
2016-11-28 21:33:49 +0100

    no message

---
+++ job-templates/mavenApplication.template.groovy
@@ -0,0 +1,14 @@
+def applicationName = inputTextParameter(
+        label:"Application Name",
+        maxLength:50,
+        tip:"The name of your application",
+        mandatory:true)
+
+def githubOrgName = inputTextParameter(
+        label:"GitHub Org Name",
+        maxLength:50,
+        tip:"The name of your GitHub Org",
+        mandatory:true)
+
+new  createApplication(applicationName, githubOrgName).build(this)
+
