3f6a2bf90e90b0acddf03e411251fc039ce0d89b
Cyrille Le Clerc <cyrille@cyrilleleclerc.com>
2016-11-29 10:38:44 +0100

    try to inject credentials + cleanup

--- src/main/groovy/com/cyrilleleclerc/jenkins/jobs/MavenApplicationJobsBuilder.groovy
+++ src/main/groovy/com/cyrilleleclerc/jenkins/jobs/MavenApplicationJobsBuilder.groovy
@@ -1,7 +1,6 @@
 package com.cyrilleleclerc.jenkins.jobs
 
 import javaposse.jobdsl.dsl.DslFactory
-import javaposse.jobdsl.dsl.Job
 
 
 class MavenApplicationJobsBuilder {
@@ -13,36 +12,39 @@ class MavenApplicationJobsBuilder {
 
         String basePath = "$rootFolder/$applicationName"
 
-        factory.println("BEFORE add folder $basePath")
-
         factory.folder(basePath) {
             description "CD Jobs for $applicationName"
         }
 
-        factory.println("BEFORE add build job")
-
-
+        // BUILD JOB
         factory.freeStyleJob("$basePath/build") {
             scm {
-                github("$githubOrgName/$applicationName")
+                github(ownerAndProject: "$githubOrgName/$applicationName", branch: 'master')
+                credentialsId("github-credentials")
             }
             triggers {
-                scm 'H/5 * * * *'
+                githubPush()
             }
             steps {
                 shell('$WORKSPACE/mvnw clean package')
             }
+            publishers {
+                archiveArtifacts('target/**/*.jar')
+                archiveJunit 'target/surefire-reports/**/TEST-*.xml'
+            }
         }
 
-        factory.println("BEFORE add release job")
-
-
+        // RELEASE JOB
         factory.freeStyleJob("$basePath/release") {
             scm {
                 github("$githubOrgName/$applicationName")
             }
             steps {
                 shell('$WORKSPACE/mvnw clean release:prepare && $WORKSPACE/mvnw clean release:perform')
+            }
+            publishers {
+                archiveArtifacts('target/**/*.jar')
+                archiveJunit 'target/surefire-reports/**/TEST-*.xml'
             }
         }
     }
